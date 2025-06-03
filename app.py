000ed61a3e79cf4098672fda617fedcdbfd83a6f
Antipas Zemba <antipasnzeba.com>
2025-06-03 01:25:19 -0700

    Initial commit - Function to display the menu options to the user.

---
+++ app.py
@@ -0,0 +1,11 @@
+# List to store tasks; each task is a dictionary with a description and completion status.
+tasks[]
+
+# Function to display the menu options to the user.
+def show_menu():
+    print("\n*** TO DO LIST ***")
+    print("1. Add Task")
+    print("2. View Task")
+    print("3. Mark Task as COmpleted")
+    print("1. Delete Task")
+    print("1. Exit")
